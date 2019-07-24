0368ed5e32e0fbbd3a710ff489cf452149e98869
lilian <lilian.stoklosa@laposte.net>
2019-07-24 14:50:27 +0200

    début projet

---
+++ Consonne/src/Controller/ConsonneController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConsonneController extends AbstractController
+{
+    /**
+     * @Route("/consonne", name="consonne")
+     */
+    public function index()
+    {
+        return $this->render('consonne/index.html.twig', [
+            'controller_name' => 'ConsonneController',
+        ]);
+    }
+}
