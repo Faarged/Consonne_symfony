421beee2129e627cc9fa4184285149c651052ffb
lilian <lilian.stoklosa@laposte.net>
2019-09-04 09:42:48 +0200

    form inscript

--- Consonne/src/Form/RegistrationType.php
+++ Consonne/src/Form/RegistrationType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 
 class RegistrationType extends AbstractType
 {
@@ -29,7 +30,13 @@ class RegistrationType extends AbstractType
               ]
             ])
             ->add('endSubAt')
-            ->add('gameTime')
+            ->add('gameTime', TimeType::class, [
+              'hours' => [01, 02],
+              'placeholder' => [
+                  'hour' => 'Heure'
+              ],
+
+            ])
             ->add('password', PasswordType::class)
             ->add('confirm_password', PasswordType::class)
             //->add('isAdmin')
